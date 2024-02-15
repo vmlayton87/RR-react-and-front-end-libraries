3659e2f6509c97c464697c1ffcef4e229334986a
vmlayton87 <vmlayton87@gmail.com>
2024-02-15 13:11:06 -0500

    final assignment

--- 7.1.3-declarative-counter/src/App.js
+++ 7.1.3-declarative-counter/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+function App(){
+  let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter
+  // counter++
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Counter = {counter}</h1>
+    <h4>Counter = {counter}</h4>
+    <p>What happens if I add ++ to counter in the html: {counter++}</p>
+    <p>The ++ activates in the previous line of html code and shows the previous value of counter, then adds one and now counter=: {counter}</p>
     </div>
-  );
-}
+  )
+  
+} 
 
-export default App;
+export default App
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+        
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
